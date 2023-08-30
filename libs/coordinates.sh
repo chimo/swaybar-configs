6fed6b05767d65b7a0275cd9bbf026349efaf1c3
chimo <chimo@chromic.org>
2023-08-30 19:36:20 -0400

    libs: Add `coordinates.sh`

---
+++ libs/coordinates.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+get_coordinates() (
+    wget -O- --header "Secret: ${LOCATION_SECRET}" \
+        "https://chromic.org/?location" -q
+)
+
+
+# This is a job for `jq` really, but I'm trying to keep things minimal and I
+# control the JSON on the other end so I know what to expect.
+# (inb4 future-self curses at me when I inevitably change the server response)
+_extract() (
+    json="${1}"
+    field="${2}"
+
+    printf '%s' "${json}" | cut -d, -f "${field}" | cut -d: -f2 | tr -d '"'
+)
+
+
+get_latitude() (
+    json="${1}"
+
+    res=$(_extract "${json}" 2)
+
+    # Remove closing curly bracket
+    printf '%s' "${res}" | sed 's/.$//'
+)
+
+
+get_longitude() (
+    json="${1}"
+
+    _extract "${json}" 1
+)
+
+
+main() (
+    json=$(get_coordinates)
+
+    lat=$(get_latitude "${json}")
+    lon=$(get_longitude "${json}")
+
+    echo "${lat},${lon}"
+)
+
+
+main
+
