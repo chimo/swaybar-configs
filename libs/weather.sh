462852a7d1dd32faaf79ed41e26f5c58eb71904f
chimo <chimo@chromic.org>
2023-09-10 22:24:50 -0400

    libs/weather: Add missing degree symbol when icon is present

--- libs/weather.sh
+++ libs/weather.sh
@@ -72,7 +72,7 @@ main() (
     if [ -z "${icon}" ]; then
         echo "${temperature}°, ${humidity}%, ${condition}"
     else
-        printf "%b %s, %s%%" "${icon}" "${temperature}" "${humidity}"
+        printf "%b %s°, %s%%" "${icon}" "${temperature}" "${humidity}"
     fi
 )
 
