c9f6ac224302d2708a1904ffde9f189decc7a025
chimo <chimo@chromic.org>
2023-11-19 14:16:24 -0500

    updates.sh: formatting

--- blocks/updates/updates.sh
+++ blocks/updates/updates.sh
@@ -41,13 +41,13 @@ _apk() (
 
 
 get_latest_alpine_version() (
-	# https://stackoverflow.com/a/12704727
-	git -c 'versionsort.suffix=-' \
-		ls-remote --exit-code --refs --sort='version:refname' \
-		--tags https://git.alpinelinux.org/aports '*.*.*' \
+    # https://stackoverflow.com/a/12704727
+    git -c 'versionsort.suffix=-' \
+        ls-remote --exit-code --refs --sort='version:refname' \
+        --tags https://git.alpinelinux.org/aports '*.*.*' \
         | grep -vE '[0-9]+\.[0-9]+_rc' \
-		| tail -n 1 \
-		| cut -d '/' -f3 \
+        | tail -n 1 \
+        | cut -d '/' -f3 \
         | grep -oE '[0-9]+\.[0-9]+'
 )
 
