049744c5d0a750553f9def2ea85b7f7ee55f7871
chimo <chimo@chromic.org>
2023-11-19 14:14:38 -0500

    updates.sh: Ignore alpine Release Candidates

--- blocks/updates/updates.sh
+++ blocks/updates/updates.sh
@@ -45,6 +45,7 @@ get_latest_alpine_version() (
 	git -c 'versionsort.suffix=-' \
 		ls-remote --exit-code --refs --sort='version:refname' \
 		--tags https://git.alpinelinux.org/aports '*.*.*' \
+        | grep -vE '[0-9]+\.[0-9]+_rc' \
 		| tail -n 1 \
 		| cut -d '/' -f3 \
         | grep -oE '[0-9]+\.[0-9]+'
